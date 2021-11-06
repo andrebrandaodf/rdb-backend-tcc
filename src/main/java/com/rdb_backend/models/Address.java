31a351dfe24c69c55e095eb578871fda2276b784
André Brandão <andrebrandaodf@gmail.com>
2021-11-05 22:58:20 -0300

    Criando entidades Donor e Address

---
+++ src/main/java/com/rdb_backend/models/Address.java
@@ -0,0 +1,170 @@
+package com.rdb_backend.models;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data	
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String cep;
+
+	@NotNull
+	private String logradouro;
+	
+	@NotNull
+	private String bairro;
+	
+	@NotNull
+	private String localidade;
+	
+	@Size(max = 2)
+	@NotNull
+	private String uf;
+	
+	@Size(max = 100)
+	private String complemento;
+	
+	private Integer ddd; 
+	
+	private String gia;
+	
+	private Integer ibge;
+   
+	private Integer siafi;
+	
+	@OneToOne(targetEntity=Donor.class, fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn
+	private Donor donor;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getLocalidade() {
+		return localidade;
+	}
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public Integer getDdd() {
+		return ddd;
+	}
+	public void setDdd(Integer ddd) {
+		this.ddd = ddd;
+	}
+	public String getGia() {
+		return gia;
+	}
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
+	public Integer getIbge() {
+		return ibge;
+	}
+	public void setIbge(Integer ibge) {
+		this.ibge = ibge;
+	}
+	public Integer getSiafi() {
+		return siafi;
+	}
+	public void setSiafi(Integer siafi) {
+		this.siafi = siafi;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", "
+				+ "cep=" + cep + ", "
+						+ "logradouro=" + logradouro + 
+						", bairro=" + bairro
+				+ ", localidade=" + localidade +
+				", uf=" + uf + 
+				", complemento=" + complemento + 
+				", donor=" + donor
+				+ "]";
+	}	
+}
---
+++ src/main/java/com/rdb_backend/models/Donor.java
@@ -0,0 +1,113 @@
+package com.rdb_backend.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="donor")
+public class Donor implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Size(min = 3, max = 100)
+	@NotNull(message = "Nome não pode ser nulo")
+	private String name;
+
+	@Size(min = 3, max = 100)
+	@NotNull(message = "Sobrenome não pode ser nulo")
+	private String surname;
+
+	@OneToOne(targetEntity= Address.class, fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private Address address;
+
+	@NotNull
+	private String phone;
+
+	@Email
+	@NotNull
+	private String email;
+	
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
+
